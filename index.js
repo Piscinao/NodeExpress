013403829dc90f09628ec747f0f7bc854b3794f6
Piscinao <henhvj@gmail.com>
2020-05-26 19:18:23 -0300

    Adicionando Rotas com parametros obrigatórios e opcionais

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ const app = express();//express função que carrega todo o framework no projeto
 
 //trabalhando com rotas chamar apenas uma requisição por rota!
 app.get("/", function(req, res){
-  res.send("Bem vindo a página inicial!");
+  res.send("<h1>Bem vindo a página inicial!</h1>");
 });
 
-app.get("/blog", function(req, res){
+//parametro opicional coloca "?"
+app.get("/blog/:artigo?", function(req, res){
+
+  //ira aparecer apenas se um parâmetro existir
+  var artigo = req.params.artigo;
+  if(artigo){
+    res.send("<h2>Artigo: " + artigo + "</h2>")
+
+  }else{
+
+  }
   res.send("Bem Vindo ao meu blog!");
 });
 
 app.get("/canal", function(req, res){
   res.send("Bem Vindo ao meu canal!");
+});
+
+app.get("/ola/:nome/:empresa", function (req, res){
+  //req => dados enviados pelo usuário
+  //res => resposta que vai ser retornada para o usuário
+  //params enviado pelo usuário
+  var nome = req.params.nome;
+  var empresa = req.params.empresa;
+  res.send("<h1> Oi " + nome + " do " + empresa + " </h1>");
 });
 
 
